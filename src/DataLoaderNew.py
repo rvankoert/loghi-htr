96c88275aaf74c8f53bd02bc4bf0dbcd6644cb3b
Martijn Maas <martijn.maas@di.huc.knaw.nl>
2022-11-07 13:04:49 +0100

    Refactor DataLoaderNew
    Create one  method for loading data

--- src/DataLoaderNew.py
+++ src/DataLoaderNew.py
@@ -88,200 +88,20 @@ class DataLoaderNew:
         chars = set()
         partition = {'train': [], 'validation': [], 'test': [], 'inference': []}
         labels = {'train': [], 'validation': [], 'test': [], 'inference': []}
-        trainLabels = {}
-        valLabels = {}
-        testLabels = {}
-        inference_labels = {}
-        print(self.train_list)
 
         if self.train_list:
-            for sublist in self.train_list.split():
-                if not os.path.exists(sublist):
-                    print(sublist + "does not exist, enter a valid filename. exiting...")
-                    exit(1)
-                f = open(sublist)
-                counter = 0
-                for line in f:
-                    # ignore comment line
-                    if not line or line[0] == '#':
-                        continue
-                    # print(line)
-                    lineSplit = line.strip().split('\t')
-                    if len(lineSplit) == 1:
-                        continue
-
-                    # filename
-                    fileName = lineSplit[0]
-                    if self.check_missing_files and not os.path.exists(fileName):
-                        print("missing: " + fileName)
-                        continue
-                    if self.normalize_text:
-                        gtText = self.normalize(lineSplit[1])
-                    else:
-                        gtText = lineSplit[1]
-                    ignoreLine = False
-                    if self.injected_charlist and not self.replace_final_layer:
-                        for char in gtText:
-                            if char not in self.injected_charlist:
-                                # print(self.injected_charlist)
-                                print('a ignoring line: ' + gtText)
-                                ignoreLine = True
-                                break
-                    if ignoreLine or len(gtText) == 0:
-                        print(line)
-                        continue
-                    counter = counter + 1
-                    for i in range(0, self.multiply):
-                        partition['train'].append(fileName)
-                        labels['train'].append(gtText)
-                        trainLabels[fileName] = gtText
-                    if not self.injected_charlist or self.replace_final_layer:
-                        chars = chars.union(set(char for label in gtText for char in label))
-                    # if counter > 100:
-                    #     break
-                print('found ' + str(counter) + ' lines suitable for training')
-                f.close()
+            chars = self.create_data(chars, labels, partition, 'train', self.train_list)
 
         if self.validation_list:
-            for sublist in self.validation_list.split():
-                if not os.path.exists(sublist):
-                    print(sublist + "does not exist, enter a valid filename. exiting...")
-                    exit(1)
-                f = open(sublist)
-                counter = 0
-                for line in f:
-                    # ignore comment line
-                    if not line or line[0] == '#':
-                        continue
-
-                    lineSplit = line.strip().split('\t')
-                    if len(lineSplit) == 1:
-                        continue
-
-                    # filename
-                    fileName = lineSplit[0]
-                    if self.check_missing_files and not os.path.exists(fileName):
-                        print("missing: " + fileName)
-                        continue
-                    if self.normalize_text:
-                        gtText = self.normalize(lineSplit[1])
-                    else:
-                        gtText = lineSplit[1]
-
-                    ignoreLine = False
-                    if self.injected_charlist and not self.replace_final_layer:
-                        for char in gtText:
-                            if char not in self.injected_charlist:
-                                # print(self.injected_charlist)
-                                print('b ignoring line: ' + gtText)
-                                ignoreLine = True
-                                break
-                    if ignoreLine or not gtText:
-                        continue
-
-                    counter = counter + 1
-                    # if (counter > 10000):
-                    #     break
-
-                    # put sample into list
-                    partition['validation'].append(fileName)
-                    labels['validation'].append(gtText)
-                    valLabels[fileName] = gtText
-                    if not self.injected_charlist or self.replace_final_layer:
-                        chars = chars.union(set(char for label in gtText for char in label))
-                print('found ' + str(counter) + ' lines suitable for validation')
-                f.close()
-
-        counter = 0
+            chars = self.create_data(chars, labels, partition, 'validation', self.validation_list)
 
         if self.test_list:
-            for sublist in self.test_list.split():
-                if not os.path.exists(sublist):
-                    print(sublist + "does not exist, enter a valid filename. exiting...")
-                    exit(1)
-                f = open(sublist)
-                for line in f:
-                    # ignore comment line
-                    if not line or line[0] == '#':
-                        continue
-
-                    lineSplit = line.strip().split('\t')
-                    if len(lineSplit) == 1:
-                        continue
-
-                    # filename
-                    fileName = lineSplit[0]
-                    if self.check_missing_files and not os.path.exists(fileName):
-                        print("missing: " + fileName)
-                        continue
-                    # img = cv2.imread(fileName)
-                    # height, width, channels = img.shape
-                    # if height < 20 or width < 100 or width / height < 4:
-                    #     print(fileName)
-                    #     os.remove(fileName)
-                    #     continue
-                    if self.normalize_text:
-                        gtText = self.normalize(lineSplit[1])
-                    else:
-                        gtText = lineSplit[1]
-
-                    if not gtText:
-                        continue
-
-                    counter = counter + 1
-                    # if (counter > 100):
-                    #     break
-
-                    # put sample into list
-                    partition['test'].append(fileName)
-                    labels['test'].append(gtText)
-                    testLabels[fileName] = gtText
-                    if not self.injected_charlist:
-                        chars = chars.union(set(char for label in gtText for char in label))
-                print('found ' + str(counter) + ' lines suitable for testing')
-                f.close()
+            chars = self.create_data(chars, labels, partition, 'test', self.test_list, include_unsupported_chars=True)
 
         if self.inference_list:
-            for sublist in self.inference_list.split():
-                if not os.path.exists(sublist):
-                    print(sublist + "does not exist, enter a valid filename. exiting...")
-                    exit(1)
-                f = open(sublist)
-                for line in f:
-                    # ignore comment line
-                    if not line or line[0] == '#':
-                        continue
+            chars = self.create_data(chars, labels, partition, 'inference', self.inference_list,
+                                     include_unsupported_chars=True, include_missing_files=True, is_inference=True)
 
-                    lineSplit = line.strip().split('\t')
-                    assert len(lineSplit) >= 1
-
-                    # filename
-                    fileName = lineSplit[0]
-                    # if not os.path.exists(fileName):
-                    #     # print(fileName)
-                    #     continue
-                    # img = cv2.imread(fileName)
-                    # height, width, channels = img.shape
-                    # if height < 20 or width < 100 or width / height < 4:
-                    #     print(fileName)
-                    #     os.remove(fileName)
-                    #     continue
-                    label = 'to be determined'
-
-                    counter = counter + 1
-                    # if (counter > 100):
-                    #     break
-                    # if counter % 10000 == 0:
-                    #     print(counter)
-
-                    # put sample into list
-                    partition['inference'].append(fileName)
-                    labels['inference'].append(label)
-                    inference_labels[fileName] = label
-                f.close()
-                if len(partition['inference']) == 0:
-                    print("no data to inference. Check your input-file")
-                    exit(1)
         # list of all chars in dataset
         if self.injected_charlist and not self.replace_final_layer:
             print('using injected_charlist')
@@ -348,6 +168,56 @@ class DataLoaderNew:
         self.partition = partition
 
         return training_generator, validation_generator, test_generator, inference_generator
+
+    def create_data(self, chars, labels, partition, partition_name, data_file_list, include_unsupported_chars=False,
+                    include_missing_files=False, is_inference=False):
+        for sublist in data_file_list.split():
+            if not os.path.exists(sublist):
+                print(sublist + "does not exist, enter a valid filename. exiting...")
+                exit(1)
+            with open(sublist) as f:
+                counter = 0
+                for line in f:
+                    # ignore comment line
+                    if not line or line[0] == '#':
+                        continue
+                    # print(line)
+                    lineSplit = line.strip().split('\t')
+                    if not is_inference and len(lineSplit) == 1:
+                        continue
+
+                    # filename
+                    fileName = lineSplit[0]
+                    if not include_missing_files and self.check_missing_files and not os.path.exists(fileName):
+                        print("missing: " + fileName)
+                        continue
+                    if is_inference:
+                        gtText = 'to be determined'
+                    elif self.normalize_text:
+                        gtText = self.normalize(lineSplit[1])
+                    else:
+                        gtText = lineSplit[1]
+                    ignoreLine = False
+                    if not include_unsupported_chars and self.injected_charlist and not self.replace_final_layer:
+                        for char in gtText:
+                            if char not in self.injected_charlist:
+                                # print(self.injected_charlist)
+                                print('a ignoring line: ' + gtText)
+                                ignoreLine = True
+                                break
+                    if ignoreLine or len(gtText) == 0:
+                        print(line)
+                        continue
+                    counter = counter + 1
+                    for i in range(0, self.multiply):
+                        partition[partition_name].append(fileName)
+                        labels[partition_name].append(gtText)
+                    if not self.injected_charlist or self.replace_final_layer:
+                        chars = chars.union(set(char for label in gtText for char in label))
+                    # if counter > 100:
+                    #     break
+                print('found ' + str(counter) + ' lines suitable for ' + partition_name)
+        return chars
 
     @staticmethod
     def truncateLabel(text, maxTextLen):
