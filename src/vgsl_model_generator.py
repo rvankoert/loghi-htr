75e5874f78e17149dd67d15b70278a844a55dc28
Luke Peters <lukepet@hotmail.com>
2023-08-17 09:08:38 +0200

    Testing adding bla bla in comment

--- src/vgsl_model_generator.py
+++ src/vgsl_model_generator.py
@@ -11,7 +11,7 @@ from tensorflow.keras import layers, models, initializers, Input
 
 
 class VGSLModelGenerator:
-    """Nice description about this class """
+    """Nice description about this class blabla"""
 
     def __init__(self,
                  model,
