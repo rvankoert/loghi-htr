cf23032ecd2c64790bf2d474f2e85762231cf263
Tim Koornstra <tim.koornstra@gmail.com>
2024-03-06 11:31:15 +0100

    Fix arg parser bug + incorrect function name

--- src/main.py
+++ src/main.py
@@ -76,7 +76,7 @@ def main():
                                         augmentation_model)
         training_dataset, evaluation_dataset, validation_dataset, \
             test_dataset, inference_dataset, _, train_batches, \
-            validation_labels = loader.generators()
+            validation_labels = loader.get_generators()
 
         # Replace the charlist with the one from the data loader
         charlist = loader.charList
